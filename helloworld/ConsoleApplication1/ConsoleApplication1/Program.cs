c85e9074d9154cb2fabde07835569c66b5fc02bb
JonathanXing <jonathan.xing17@gmail.com>
2014-04-15 13:08:43 +1000

    HelloWorld

---
+++ helloworld/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using ClearScada.Client;
+using ClearScada.Client.Simple;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (Connection conn = new Connection("TMC"))
+        {
+            // Connect to local server
+            conn.Connect("192.168.1.104");
+
+            // Create a new point
+            DBObject point = conn.CreateObject("CPointAlgManual", ObjectId.Root, "My Point");
+
+            // Set a property
+            point["FullScale"] = 200;
+
+            // Get a property
+            Console.WriteLine("FullScale = {0}", point["FullScale"]);
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
